32d73652cafc7ea4871b131fe92111446d579125
Luca8991 <luca.berte98@hotmail.it>
2020-05-16 19:39:12 +0200

    relative import urdf path

--- edo/implementation/simulator_edo/matlab/init_3link_planar.m
+++ edo/implementation/simulator_edo/matlab/init_3link_planar.m
@@ -2,11 +2,11 @@ clear all
 close all
 
 % Load robot object from URDF
-robot_3link_planar = importrobot('/home/eulero/Documents/ros/edo_impl/simulator_edo/robots/edo_sim.urdf');
+robot_3link_planar = importrobot('../robots/edo_sim.urdf');
 
 % Parameters (same as in URDF)
 L0 = 0.145292221848281; % base_link [m]
-%L1 = 0.18967; % link_1 [m]
+%L1 = 0.18967; % link_1 [m] (old)
 L1 = 0.334962221848281; % link_1 [m]
 L2 = 0.210497555007178; % link_2 [m]
 L3 = 0.427990120537185; % link_3 (=link_3 + link_4 + link_5 + link_6)[m]
